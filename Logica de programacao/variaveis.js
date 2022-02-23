aa674ab24403121f63db3b6faad5bf463cef41ac
Matheus1199 <89140928+Matheus1199@users.noreply.github.com>
2022-02-23 12:40:41 -0300

    a

---
+++ Logica de programacao/variaveis.js
@@ -0,0 +1,14 @@
+var um_numero = 50;
+var outro_numero = "50min";
+
+um_numero = 5;
+
+document.write(typeof(um_numero));
+document.write("<br>");
+document.write(outro_numero);
+document.write("<br>");
+document.write(outro_numero * 2);
+document.write("<br>");
+document.write(um_numero == 6);
+document.write("<br>");
+document.write(typeof(um_numero == 6));
