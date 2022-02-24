530b9170655fc60c33d2b95d834ce2b758e866ba
Matheus1199 <89140928+Matheus1199@users.noreply.github.com>
2022-02-24 09:45:56 -0300

    Update oo_classes.js

--- Topico 3 - Orientacao a objeto/oo_classes.js
+++ Topico 3 - Orientacao a objeto/oo_classes.js
@@ -1,5 +1,13 @@
-class Pessoa(){
-    contructor(nome){
+class Pessoa{
+    constructor(nome) {
         this.nome = nome;
     }
+
+    falar(frase) {
+        console.log(this.nome + " disse: " + frase);
+    }
 }
+
+var p1 = new Pessoa("Francisco");
+p1.falar("Oi, tudo bem?")
+p1.falar("Como vai?")
